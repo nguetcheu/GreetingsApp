4c652417b991e317743c69fa44cd8db8ffbff44a
nguetcheu <dnguetcheu@gmail.com>
2024-11-16 21:58:27 +0100

    Création du listener et ajout de commentaire

--- app/src/main/java/com/example/greetingsapp/MainActivity.java
+++ app/src/main/java/com/example/greetingsapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.greetingsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Création des variables des composants
+        EditText myEditText = findViewById(R.id.myEditText1);
+        Button myButton = findViewById(R.id.myBtn1);
+
+        // Création du listener pour le bouton
+        View.OnClickListener btnListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Button button = (Button) v;
+               String result = myEditText.getText().toString();
+               Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+            }
+        };
     }
 }
